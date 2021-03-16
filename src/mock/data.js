49426f5b92f76d8a34ecdfd96ddcb4a7ddd0fa5f
Quentin PLAUD <qplaud@gmail.com>
2021-03-16 09:15:29 +0100

    Fullstack instead of full stack

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: "",
   name: "Quentin",
-  subtitle: "I'm a Full Stack Web Developer",
+  subtitle: "I'm a Fullstack Web Developer",
   cta: "",
 };
 
